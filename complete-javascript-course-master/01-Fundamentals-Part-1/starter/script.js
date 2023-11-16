72a1c66bc4fe8430453a3875a6f5748072e34bb8
Albert O <143290938+zybertobeto@users.noreply.github.com>
2023-11-16 04:39:47 +0000

    Began coding fundamentals

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,44 @@
+ let js = "amazing"
+ console.log(40 + 38 + 93 - 47);
+
+ console.log(23);
+ 
+ // Variable name conventions
+ let firstName = "Jonathan";
+ console.log(firstName);
+
+ let PI = 3.1415;
+
+ let myFirstJob = "Teacher";
+ let myCurrentJob = "Programmer";
+ 
+ console.log(myFirstJob);
+ console.log(myCurrentJob);
+
+/*
+ let js = "amazing"
+ console.log(40 + 38 + 93 - 47);
+
+ console.log(23);
+ 
+ // Variable name conventions
+ let firstName = "Jonathan";
+ console.log(firstName);
+
+ let PI = 3.1415;
+
+ let myFirstJob = "Teacher";
+ let myCurrentJob = "Programmer";
+ 
+ console.log(myFirstJob);
+ console.log(myCurrentJob);
+ */
+
+ let javascriptIsFun = true;
+ console.log(javascriptIsFun);
+
+ console.log(typeof true);
+ console.log(typeof javascriptIsFun);
+ console.log(typeof 23);
+ console.log(typeof 'Jonathan');
+ 
