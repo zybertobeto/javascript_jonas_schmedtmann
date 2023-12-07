0f88d0d261937e3874cd43a022e3948787c8efa4
Albert O <143290938+zybertobeto@users.noreply.github.com>
2023-12-07 01:56:13 +0000

    Modified folder

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,19 +1,19 @@
- let js = "amazing"
- console.log(40 + 38 + 93 - 47);
+let js = "amazing";
+console.log(40 + 38 + 93 - 47);
 
- console.log(23);
- 
- // Variable name conventions
- let firstName = "Jonathan";
- console.log(firstName);
+console.log(23);
 
- let PI = 3.1415;
+// Variable name conventions
+let firstName = "Jonathan";
+console.log(firstName);
 
- let myFirstJob = "Teacher";
- let myCurrentJob = "Programmer";
- 
- console.log(myFirstJob);
- console.log(myCurrentJob);
+let PI = 3.1415;
+
+let myFirstJob = "Teacher";
+let myCurrentJob = "Programmer";
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
 
 /*
  let js = "amazing"
@@ -34,11 +34,10 @@
  console.log(myCurrentJob);
  */
 
- let javascriptIsFun = true;
- console.log(javascriptIsFun);
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
 
- console.log(typeof true);
- console.log(typeof javascriptIsFun);
- console.log(typeof 23);
- console.log(typeof 'Jonathan');
- 
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonathan");
